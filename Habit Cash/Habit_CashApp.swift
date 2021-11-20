6812dd7b1941482356e8ed1d7fd50bf712580484
HunterHab <hunterhabersaat@gmail.com>
2021-11-19 20:14:00 -0500

    Initial Commit

---
+++ Habit Cash/Habit_CashApp.swift
@@ -0,0 +1,17 @@
+//
+//  Habit_CashApp.swift
+//  Habit Cash
+//
+//  Created by Hunter Habersaat on 11/19/21.
+//
+
+import SwiftUI
+
+@main
+struct Habit_CashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
